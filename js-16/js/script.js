0605472385c086bf3f5a4c46c2b1488e50400af9
Den <den_cimbal@mail.ru>
2016-08-31 10:47:48 +0300

    add script

---
+++ js-16/js/script.js
@@ -0,0 +1,54 @@
+$(function() {  
+    $('#search').on('click', function(){
+        $('.img-result').remove();
+        var API_KEY = '3194517-4b828b06718630aa5859376c4';
+        var URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent($('#query').val());
+        $.getJSON(URL, function(data){
+            if (parseInt(data.totalHits) > 0)
+                $.each(data.hits, function(i, hit){ 
+                    $('.result').append('<a class="img-result" target="_blank" href="' + hit.pageURL + '"><img src="' + hit.previewURL + '"></a>');
+                });
+            else
+                console.log('No hits');
+        });
+    });
+    
+    function Human() {
+        this.name = 'Paul',
+        this.age = 30,
+        this.gender = 'male',
+        this.height = 170,
+        this.weight = 50
+    };
+    
+    var newHuman = new Human();
+    
+    function Worker() {
+        this.work = function() {
+            this.work = 'Driver',
+            this.salary = 500
+        }
+    };
+    
+    var newWorker = new Worker();
+    var newWorker1 = new Worker();
+    
+    function Student() {
+        this.WatchSerials = function() {
+            this.education = 'school',
+            this.grant = 200
+        }  
+    };
+    
+    var newStudent = new Student();
+    var newStudent1 = new Student();
+    
+    
+    Worker.prototype = newHuman;
+    Student.prototype = newHuman;
+    
+    console.log(newWorker);
+    console.log(newWorker1);
+    console.log(newStudent);
+    console.log(newStudent1);
+});
