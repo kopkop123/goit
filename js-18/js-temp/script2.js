1f4d92507a7b7f0e96f6382d7b0686d4ad2f7a4c
Den <den_cimbal@mail.ru>
2016-09-01 12:05:50 +0300

    added grunt

--- js-18/js-temp/script2.js
+++ js-18/js-temp/script2.js
@@ -1,18 +1,4 @@
-$(function() {  
-    $('#search').on('click', function(){
-        $('.img-result').remove();
-        var API_KEY = '3194517-4b828b06718630aa5859376c4';
-        var URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent($('#query').val());
-        $.getJSON(URL, function(data){
-            if (parseInt(data.totalHits) > 0)
-                $.each(data.hits, function(i, hit){ 
-                    $('.result').append('<a class="img-result" target="_blank" href="' + hit.pageURL + '" title="' + hit.pageURL + '"><img src="' + hit.previewURL + '"></a>');
-                });
-            else
-                console.log('No hits');
-        });
-    });
-    
+$(function() {      
     function Human() {
         this.name = 'Paul',
         this.age = 30,
