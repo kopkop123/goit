8e0b3e7b724f644d93bc17341c06021877f81f42
Den <den_cimbal@mail.ru>
2016-10-10 14:34:55 +0300

    fix js

--- js-exam/js/script.js
+++ js-exam/js/script.js
@@ -10,12 +10,18 @@ $(function(){
         $('#slider1_' + count).addClass('hide').removeClass('show-cell');
         $('#slider1_' + ++count).addClass('show-cell').removeClass('hide');
         if(count == 4) { count = 1; $('#slider1_1').addClass('show-cell').removeClass('hide'); }
+        if ($('#slider1_1').hasClass('show-cell')) { $(this).closest('.steps__block').removeClass('steps__block--2 steps__block--3').addClass('steps__block--1'); }
+        else if ($('#slider1_2').hasClass('show-cell')) { $(this).closest('.steps__block').removeClass('steps__block--1 steps__block--3').addClass('steps__block--2'); }
+        else { $(this).closest('.steps__block').removeClass('steps__block--1 steps__block--2').addClass('steps__block--3'); }
     });
 
     $('#arrows1 .steps__arrows--left').on('click', function(){
         $('#slider1_' + count).addClass('hide').removeClass('show-cell');
         $('#slider1_' + --count).addClass('show-cell').removeClass('hide');
         if(count == 0) { count = 3; $('#slider1_3').addClass('show-cell').removeClass('hide') }
+        if ($('#slider1_1').hasClass('show-cell')) { $(this).closest('.steps__block').removeClass('steps__block--2 steps__block--3').addClass('steps__block--1'); }
+        else if ($('#slider1_2').hasClass('show-cell')) { $(this).closest('.steps__block').removeClass('steps__block--1 steps__block--3').addClass('steps__block--2'); }
+        else { $(this).closest('.steps__block').removeClass('steps__block--1 steps__block--2').addClass('steps__block--3'); }
     });
 
     // Слайдер#2
@@ -25,12 +31,18 @@ $(function(){
 		$('#slider2_' + count2).addClass('hide').removeClass('show-cell');
 		$('#slider2_' + ++count2).addClass('show-cell').removeClass('hide');
 		if(count2 == 4) { count2 = 1; $('#slider2_1').addClass('show-cell').removeClass('hide'); }
+        if ($('#slider2_1').hasClass('show-cell')) { $(this).closest('.steps__block').removeClass('steps__block--1 steps__block--3').addClass('steps__block--2'); }
+        else if ($('#slider2_2').hasClass('show-cell')) { $(this).closest('.steps__block').removeClass('steps__block--1 steps__block--2').addClass('steps__block--3'); }
+        else { $(this).closest('.steps__block').removeClass('steps__block--3 steps__block--2').addClass('steps__block--1'); }
 	});
 
 	$('#arrows2 .steps__arrows--left').on('click', function(){
 		$('#slider2_' + count2).addClass('hide').removeClass('show-cell');
 		$('#slider2_' + --count2).addClass('show-cell').removeClass('hide');
 		if(count2 == 0) { count2 = 3; $('#slider2_3').addClass('show-cell').removeClass('hide') }
+        if ($('#slider2_1').hasClass('show-cell')) { $(this).closest('.steps__block').removeClass('steps__block--1 steps__block--3').addClass('steps__block--2'); }
+        else if ($('#slider2_2').hasClass('show-cell')) { $(this).closest('.steps__block').removeClass('steps__block--1 steps__block--2').addClass('steps__block--3'); }
+        else { $(this).closest('.steps__block').removeClass('steps__block--3 steps__block--2').addClass('steps__block--1'); }
 	});
 	// Слайдер#3
 	$('#slider3_1').addClass('show-cell').removeClass('hide');
@@ -39,12 +51,18 @@ $(function(){
 		$('#slider3_' + count3).addClass('hide').removeClass('show-cell');
 		$('#slider3_' + ++count3).addClass('show-cell').removeClass('hide');
 		if(count3 == 4) { count3 = 1; $('#slider3_1').addClass('show-cell').removeClass('hide'); }
+        if ($('#slider3_1').hasClass('show-cell')) { $(this).closest('.steps__block').removeClass('steps__block--1 steps__block--3').addClass('steps__block--3'); }
+        else if ($('#slider3_2').hasClass('show-cell')) { $(this).closest('.steps__block').removeClass('steps__block--2 steps__block--3').addClass('steps__block--1'); }
+        else { $(this).closest('.steps__block').removeClass('steps__block--3 steps__block--2').addClass('steps__block--2'); }
 	});
 
 	$('#arrows3 .steps__arrows--left').on('click', function(){
 		$('#slider3_' + count3).addClass('hide').removeClass('show-cell');
 		$('#slider3_' + --count3).addClass('show-cell').removeClass('hide');
 		if(count3 == 0) { count3 = 3; $('#slider3_3').addClass('show-cell').removeClass('hide') }
+        if ($('#slider3_1').hasClass('show-cell')) { $(this).closest('.steps__block').removeClass('steps__block--2 steps__block--3').addClass('steps__block--1'); }
+        else if ($('#slider3_2').hasClass('show-cell')) { $(this).closest('.steps__block').removeClass('steps__block--1 steps__block--3').addClass('steps__block--2'); }
+        else { $(this).closest('.steps__block').removeClass('steps__block--1 steps__block--2').addClass('steps__block--3'); }
 	});
     
     // Поиск 
