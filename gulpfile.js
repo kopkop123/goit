6b747d29cf2ec3ab21d5f7db42138741a6c154c4
Den <den_cimbal@mail.ru>
2016-10-17 16:22:42 +0300

    add sprite

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 const gulp = require('gulp');
 var sass = require('gulp-sass');
 var imagemin = require('gulp-imagemin');
+var spritesmith = require('gulp.spritesmith');
 
 //gulp.task('sass', function () {
 //  return gulp.src('js-exam/css/*.scss')
@@ -18,4 +19,13 @@ gulp.task('compress', function() {
  
 gulp.task('sass:watch', function () {
   gulp.watch('js-exam/css/*.scss', ['sass']);
+});
+
+gulp.task('sprite', function () {
+  var spriteData = gulp.src('js-exam/img/images-for-sprite/*.png').pipe(spritesmith({
+    imgName: 'sprite.png',
+    cssName: 'sprite.less'
+  }));
+  spriteData.img.pipe(gulp.dest('js-exam/img/'));
+  spriteData.css.pipe(gulp.dest('js-exam/css/'));
 });
